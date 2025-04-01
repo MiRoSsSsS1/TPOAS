f9ad3d690ce48a93d69387a7cc129bd1efb159ed
MiRoSsSsS1 <miroslav.arapov@mail.ru>
2025-04-01 20:50:26 +0700

    Update Otrezok.cs

--- Otrezok.cs
+++ Otrezok.cs
@@ -1,1 +1,10 @@
-
+namespace TPOAS_1
+{
+    public class Otrezok
+    {
+        public double Len(double x1, double y1, double x2, double y2)
+        {
+            return Math.Round(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)), 2);
+        }
+    }
+}
